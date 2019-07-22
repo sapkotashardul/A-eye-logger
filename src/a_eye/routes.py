20093d0cb1dd013d7803be7de18ff3a7c81e7c55
AristeeThevenon <aristee.thevenon@gmail.com>
2019-07-22 02:09:57 +0200

    Added timeline, collapse down menus

--- src/a_eye/routes.py
+++ src/a_eye/routes.py
@@ -3,22 +3,64 @@ from a_eye import app,db
 from a_eye.forms import LoginForm
 from a_eye.models import Studies, Sequences
 from flask_login import login_user, current_user, logout_user
+from sqlalchemy import distinct,func
+import datetime,time
 
 #key ='68b7ecb4fe9a77eb37642a26c4f39370'
 
-posts={
-	'author' : 'lkj',
-	'date_posted' :'lkj',
-	'content' :'oiejrz',
-	'title' : 'lkjer'
-}
+
 
 @app.route("/dashboard")
 def dashboard():
 	if current_user.is_authenticated:
+		pourcentage={
+			'relaxed' : 0,
+			'lowCognitiveLoad' : 0,
+			'highCognitiveLoad' : 0,
+			'noFocusPoint' : 0,
+			'oneFocusPoint' : 0,
+			"multipleFocusPoint" : 0
+		}
+
+		sequence_datas=[]
 		sequences = Sequences.query.filter_by(author=current_user)
-		print(sequences)
-	return render_template('dashboard.html', sequences=sequences)
+		pourcentage['relaxed'] = sequences.filter_by(cognitiveLoad=0).count()
+		pourcentage['lowCognitiveLoad'] = sequences.filter_by(cognitiveLoad=1).count()
+		pourcentage['highCognitiveLoad'] = sequences.filter_by(cognitiveLoad=2).count()
+		pourcentage['noFocusPoint'] = sequences.filter_by(focusPoint=0).count()
+		pourcentage['oneFocusPoint'] = sequences.filter_by(focusPoint=1).count()
+		pourcentage['multipleFocusPoint'] = sequences.filter_by(focusPoint=2).count()
+
+		s = Sequences.query.filter_by(author=current_user).group_by(Sequences.scene)
+		for n in s:
+			dico={}
+			dico['category_scene']=n.scene
+			nb_sequence=sequences.filter_by(scene=n.scene).count()
+			category_sequence=sequences.filter_by(scene=n.scene)
+			dico['category_nb_sequences']=nb_sequence
+			list_date = []
+			for seq in category_sequence :
+				ss=[]
+				ss.append(n.scene)
+				ss.append(int(time.mktime(seq.startingDate.timetuple())) * 1000)
+				ss.append(int(time.mktime(seq.endingDate.timetuple())) * 1000)
+				ss.append(seq.cognitiveLoad)
+				ss.append(seq.focusPoint)
+				list_date.append(ss)
+			dico['category_dates_sequences'] = list_date
+			dico['category_startingDate'] = int(time.mktime(current_user.startingDate.timetuple())) * 1000
+			dico['category_endingDate'] = int(time.mktime(current_user.endingDate.timetuple())) * 1000
+			dico['category_relaxed']=sequences.filter_by(scene=n.scene).filter_by(cognitiveLoad=0).count()
+			dico['category_lowCognitiveLoad']=sequences.filter_by(scene=n.scene).filter_by(cognitiveLoad=1).count()
+			dico['category_highCognitiveLoad']=sequences.filter_by(scene=n.scene).filter_by(cognitiveLoad=2).count()
+			dico['category_noFocusPoint']=sequences.filter_by(scene=n.scene).filter_by(focusPoint=0).count()
+			dico['category_oneFocusPoint']=sequences.filter_by(scene=n.scene).filter_by(focusPoint=1).count()
+			dico['category_multipleFocusPoint']=sequences.filter_by(scene=n.scene).filter_by(focusPoint=2).count()
+			
+			sequence_datas.append(dico)
+
+    		
+	return render_template('dashboard.html', sequences=sequences, pourcentage=pourcentage,sequence_datas=sequence_datas)
 
 
 @app.route("/login", methods=['GET', 'POST'])
